10eb8af66e894ecf4a1af6a57c402794c6f9842b
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-05-25 14:46:01 +0530

    typo fix

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const constant = {
   HEADER: {
     TITLE:
-      'Hardworking web developer with a flair for creating elegant solutions in the best efficient way.',
+      'Hardworking front end developer with a flair for creating elegant solutions in the best efficient way.',
     WORK:
       'Developed Admin portal, OAuth application, Event Registration portal, E-branding website and Smart Campus Application.',
     PLAN:
