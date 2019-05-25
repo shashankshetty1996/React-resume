a3be4a694ba522efde3d5eeade0f8563db3d3510
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-05-25 14:45:07 +0530

    Styled summary section

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CONSTANTS from '../../constants';
 
+import './Summary.scss';
+
 const Header = () => {
   const { TITLE, WORK, PLAN, FOLLOW_UP } = CONSTANTS.HEADER;
   return (
-    <header>
+    <header className="summary">
       <p>
         {TITLE} {WORK} {PLAN} {FOLLOW_UP}{' '}
       </p>
