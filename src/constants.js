11d89658008554f555413a60c81bfd8684dfe09d
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-05-26 17:13:57 +0530

    Add details in experience

--- src/constants.js
+++ src/constants.js
@@ -16,9 +16,14 @@ const constant = {
       toDate: 'present',
       subTitle: 'Y Media Labs, Front end Developer.',
       infos: [
-        'Trained co worker with react and redux concept in a span of a week.',
+        'Created dynamic page component rendering, based on a JSON object, I have rendered carousel, list view and table view in. Further more this separates it into catagories for rendering and also register functions for API calls based on scenario such as initial data fetch and pagination calls.',
+        'Customized dashboard has user preference option, which gets user back to the view which he/she was previously viewing. This implementation has significantly increased overall speed by a large margin, from about 3 minutes rendering to 3 to 5 second completely.',
+        "Worked on admin portal which has private resource data, which are to be rendered based on user's accessability. For this application we have also implemented OAuth. Project is build on intensive use of storybook.",
+        'Created E-Mail and PDF template for report generation, which has more intuitive representation of data, which users.',
+        'Created and introduced firebase authentication, which increased delivery by 20%. Overall product has gained more then 120% traffic for the website.',
         'Introduced and implemented docker based deployment and importance in Software development process.',
-        'Initiated project in Vue.js. which in turn forecasted future projects in Vue.js.'
+        'Initiated project in Vue.js. which in turn forecasted future projects in Vue.js.',
+        'Trained co worker with react and redux concept in a span of one week.'
       ]
     },
     {
@@ -30,7 +35,11 @@ const constant = {
       infos: [
         'Designed architecture and database modelling for projects such as smart campus, exam evaluation portal and fest registration portal.',
         'Trained over 140+ students with various technology such as HTML, CSS, JavaScript, AngularJS, NodeJs, etc.',
-        'Organized and participated in various event such as Introduction to web technology, Hands on session on Software life cycle using DevOps technologies.'
+        'Developed application which could with stand heavy traffic for very low performing system. That had to currently handle more then 3000 concurrent user accessing shared data',
+        'Collaborated with very domain and expertise level co-worker to complete various task such as IoT, Agriculture domain, college administrator, retired bank official',
+        'Have solved various issue and security threat for various projects. Which helped product with there confidently data and prevention for external threats',
+        'Organized and participated in various event such as Introduction to web technology, Hands on session on Software life cycle using DevOps technologies.',
+        'Have lead client interaction for understanding use case and tried to understand potential significance purpose of the product and come up with optimize solution for it.'
       ]
     },
     {
@@ -41,8 +50,8 @@ const constant = {
       subTitle: 'Freelancing',
       infos: [
         'Designed full application stuck from starch and revamped existing e-branching websites.',
-        'Build end-to-end user desktop application for store to manage grocery.',
-        "Also another desktop Application to keep track of book and it's borrowers.",
+        'Build end-to-end user desktop application for store to manage grocery. Which increased saving by 3 times and overall revenue by 2 times.',
+        "Also another desktop Application to keep track of book and it's borrowers. Which had capability of tracking down and searching throw 45,000+ records. This has saved months of work all together.",
         'Cloud based data migration project for association for there payroll data management.'
       ]
     }
