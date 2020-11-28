11a88d3ed0fb3356ae5ee165a41f5bd68a9b62d8
Shashank Shetty <smile.shashank.s.shetty1996@gmail.com>
2020-11-28 17:35:56 +0530

    Adding certification section

--- src/App.js
+++ src/App.js
@@ -6,49 +6,44 @@ import SideBar from "./layout/SideBar/SideBar";
 import MainContent from "./layout/MainContent/MainContent";
 
 import {
-  Certification,
-  Experience,
-  Header,
-  SideBarSection,
-  Summary,
+	Certification,
+	Experience,
+	Header,
+	SideBarSection,
+	Summary,
 } from "./components";
 
 import CONSTANTS from "./constants";
 
 const App = () => {
-  let classNames = "App row";
-  if (window.location.pathname !== "/") {
-    classNames = classNames + " A4";
-  }
+	const {
+		PERSONAL: { INFO, EDUCATION },
+		SKILLS,
+		AWARDS,
+	} = CONSTANTS;
 
-  const {
-    PERSONAL: { INFO, EDUCATION },
-    SKILLS,
-    AWARDS,
-  } = CONSTANTS;
+	const SideBarList = [
+		{ id: 1, title: "Personal Info", data: INFO },
+		{ id: 2, title: "Skills", data: SKILLS },
+		{ id: 3, title: "Rewards And Recognition", data: AWARDS },
+		{ id: 4, title: "Education", data: EDUCATION },
+	];
 
-  const SideBarList = [
-    { id: 1, title: "Personal Info", data: INFO },
-    { id: 2, title: "Skills", data: SKILLS },
-    { id: 3, title: "Rewards And Recognition", data: AWARDS },
-    { id: 4, title: "Education", data: EDUCATION },
-  ];
-
-  return (
-    <div className={classNames}>
-      <SideBar className="side-bar">
-        <Header />
-        {SideBarList.map((list) => (
-          <SideBarSection key={list.id} title={list.title} data={list.data} />
-        ))}
-      </SideBar>
-      <MainContent className="main-contain p-1">
-        <Summary />
-        <Experience />
-        <Certification />
-      </MainContent>
-    </div>
-  );
+	return (
+		<div className="App row A4">
+			<SideBar className="side-bar">
+				<Header />
+				{SideBarList.map((list) => (
+					<SideBarSection key={list.id} title={list.title} data={list.data} />
+				))}
+			</SideBar>
+			<MainContent className="main-contain p-1">
+				<Summary />
+				<Experience />
+				<Certification />
+			</MainContent>
+		</div>
+	);
 };
 
 export default App;
--- src/components/SideBarSection/SideBarSection.js
+++ src/components/SideBarSection/SideBarSection.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import { ContentBlock, SectionTitle } from '../../common';
+import { ContentBlock, SectionTitle } from "../../common";
 
-import './SideBarSection.scss';
-
-const SideBarSection = props => {
-  const { title, data } = props;
-  return (
-    <section className="side-bar-section">
-      <SectionTitle title={title} fromSideBar />
-      <article>
-        {data.map((value, index) => (
-          <ContentBlock key={index} data={value} />
-        ))}
-      </article>
-    </section>
-  );
+const SideBarSection = (props) => {
+	const { title, data } = props;
+	return (
+		<section className="my-1">
+			<SectionTitle title={title} fromSideBar />
+			<article>
+				{data.map((value, index) => (
+					<ContentBlock key={index} data={value} />
+				))}
+			</article>
+		</section>
+	);
 };
 
 SideBarSection.propTypes = {
-  title: PropTypes.string.isRequired,
-  data: PropTypes.array.isRequired
+	title: PropTypes.string.isRequired,
+	data: PropTypes.array.isRequired,
 };
 
 export default SideBarSection;
